8ae0130d4548b5c5ca486f65af3041a08e4c449d
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2023-02-26 17:38:28 +0100

    Added animation when switching between login and sign up forms

--- src/routes/NavigationRoutes.tsx
+++ src/routes/NavigationRoutes.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import { GuestRoute, PrivateRoute } from "../auth";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
+import { GuestRoute, PrivateRoute } from "../auth";
 import { routePaths } from "../constants";
 import HomePage from "../pages/HomePage/HomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
@@ -10,23 +11,26 @@ import GuestTemplate from "../templates/GuestTemplate/GuestTemplate";
 import PrivateTemplate from "../templates/PrivateTemplate/PrivateTemplate";
 
 const NavigationRoutes = () => {
+  const location = useLocation();
   return (
     <Suspense fallback={null}>
-      <Routes>
-        // Private pages
-        <Route element={<PrivateRoute />}>
-          <Route element={<PrivateTemplate />}>
-            <Route path={routePaths.home} element={<HomePage />} />
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          // Private pages
+          <Route element={<PrivateRoute />}>
+            <Route element={<PrivateTemplate />}>
+              <Route path={routePaths.home} element={<HomePage />} />
+            </Route>
           </Route>
-        </Route>
-        // Guest pages
-        <Route element={<GuestRoute />}>
-          <Route element={<GuestTemplate />}>
-            <Route path={routePaths.login} element={<LoginPage />} />
-            <Route path={routePaths.signup} element={<SignUpPage />} />
+          // Guest pages
+          <Route element={<GuestRoute />}>
+            <Route element={<GuestTemplate />}>
+              <Route path={routePaths.login} element={<LoginPage />} />
+              <Route path={routePaths.signup} element={<SignUpPage />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </AnimatePresence>
     </Suspense>
   );
 };
